f4e5cdd1c5fae68404d579da326976fd1d9de442
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-09-05 21:27:44 +0000

    Completed Coding Lab: If Statement

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,12 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+        if (bool==true){
+            return x;
+        }
+        if (bool==false){
+            return y;
+        }
         return 0;
     }
 }
